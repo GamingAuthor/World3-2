50705f3907a3b3212f59690033a3ad250d031a8b
EricrPark <league@WTS13.attlocal.net>
2018-06-27 16:57:44 -0700

    To Do: Finish To Do list!
    almost there!

--- src/intro_to_file_io/ToDoListTracker.java
+++ src/intro_to_file_io/ToDoListTracker.java
@@ -2,12 +2,21 @@ package intro_to_file_io;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ToDoListTracker implements ActionListener {
+	HashMap<Integer, String> ash = new HashMap<Integer, String>();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton add = new JButton("Add task");
@@ -33,11 +42,85 @@ public class ToDoListTracker implements ActionListener {
 		remove.addActionListener(this);
 		save.addActionListener(this);
 		load.addActionListener(this);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/intro_to_file_io/ToDoList.txt"));
+			String line = br.readLine();
+			while(line != null){	
+				int space = line.indexOf(" ");
+				String a = line.substring(0, space);
+				int revive = Integer.parseInt(a);
+				String b = line.substring(space+1,line.length());
+				ash.put(revive, b);
+				line = br.readLine();
 		}
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource().equals(add)) {
+			String task = JOptionPane.showInputDialog("Add a task to list.");
+			String in = JOptionPane.showInputDialog("Enter an ID number for that task.");
+			int order = Integer.parseInt(in);
+			ash.put(order, task);
+		}
+		if (e.getSource().equals(remove)) {
+			String aids = JOptionPane.showInputDialog("Enter an ID number.");
+			int aid = Integer.parseInt(aids);
+			if (ash.containsKey(aid)) {
+				ash.remove(aid);
+				JOptionPane.showMessageDialog(null, "Task successfully removed.");
+			} else {
+				JOptionPane.showMessageDialog(null, "That task does not exist.");
+			}
+		}
+		if(e.getSource().equals(save)) {
+			System.out.println(ash);
+			FileWriter fw = null;
+			try {
+				fw = new FileWriter("src/intro_to_file_io/ToDoList.txt",true);
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+			for(Integer i : ash.keySet()) {
+				try {
+					fw.write(i+" "+ash.get(i)+"\n");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			try {
+				fw.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		if(e.getSource().equals(load)) {
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("src/intro_to_file_io/ToDoList.txt"));
+				String line = br.readLine();
+				while(line != null){	
+					JOptionPane.showMessageDialog(null, line);
+					line = br.readLine();
+				}
+				br.close();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 	}
 }
